12705925124f5cddffe5a4b6737a8456f751acc6
qiqipower029 <tjqjasmine1029@gmail.com>
2020-11-16 01:29:14 -0600

    code update

--- results_scenarios/Exponential Scenarios.R
+++ results_scenarios/Exponential Scenarios.R
@@ -55,3 +55,89 @@ init = list(pi = as.numeric(table(cluster.kmeans$cluster)/n)[order(cluster.kmean
 ptm = proc.time()
 mixture_exponential(y, init)
 proc.time() - ptm
+
+
+#---------------------EM function for Rayleigh --------------------#
+mixture_rayleigh = function(y, init, tol = 1e-06, max.iter = 100000000000) {
+  n = length(y)
+  k = length(init$pi)
+  
+  # E-step
+  newpi    = init$pi
+  newsigma = init$sigma
+  
+  # M-step
+  pi = numeric(k)
+  sigma = numeric(k)
+  
+  iter = 0
+  
+  while ((any(abs(sigma - newsigma)/abs(sigma) > tol) | any(abs(pi - newpi)/abs(pi) > tol)) & (iter<max.iter)) {
+    # E-step
+    pi = newpi
+    sigma = newsigma
+    w = t(replicate(n, pi)) * outer(y, sigma, drayleigh) / replicate(k, rowSums(t(replicate(n, pi)) * outer(y, sigma, drayleigh)))
+    # M-step
+    newpi = colSums(w)/n
+    newsigma = sqrt(colSums(w*y^2)/2/colSums(w))
+    
+    iter = iter + 1
+    
+    cat('Iteration: ', iter, '\n')
+  }
+  
+  # log likelihood
+  w = t(replicate(n, newpi)) * outer(y, newsigma, drayleigh) / replicate(k, rowSums(t(replicate(n, newpi)) * outer(y, newsigma, drayleigh)))
+  v = log(t(replicate(n, newpi)) * outer(y, newsigma, drayleigh))
+  loglik = sum(w * log(t(replicate(n, newpi)) * outer(y, newsigma, drayleigh)), na.rm = T)
+  
+  return(list(n = n, k = k, iter = iter, loglikelihood = loglik, init = init, pi = newpi, sigma = newsigma, max.iter = max.iter))
+}
+
+
+#---------------------------EM function for Poisson--------------------------#
+mixture_poisson = function(y, init, tol = 1e-06, max.iter = 100000000) {
+  n = length(y)
+  k = length(init$pi)
+  
+  # E-step
+  pi_after      = init$pi
+  lambda_after  = init$lambda
+  
+  # M-step
+  pi_before     = numeric(k)
+  lambda_before = numeric(k)
+  
+  iter = 0
+  while ((any(abs(lambda_before - lambda_after)/(lambda_before + 1e-04) > tol) | any(abs(pi_before - pi_after)/(pi_before + 1e-04) > tol)) & iter < max.iter) {
+    # E-step
+    pi_before = pi_after
+    lambda_before = lambda_after
+    
+    # Find matrix w
+    # method 1
+    # w = outer(1:n, 1:k, Vectorize(function(i, j) (pi_before[j] * dpois(y[i], lambda_before[j]))/sum(pi_before * dpois(y[i], lambda_before))))
+    # method 2
+    # w = matrix(nrow = n, ncol = k)
+    # for (i in 1:n) {
+    #   for (j in 1:k) {
+    #     w[i,j] = (pi_before[j] * dpois(y[i], lambda_before[j]))/sum(pi_before * dpois(y[i], lambda_before))
+    #   }
+    # }
+    # method 3
+    w = t(replicate(n, pi_before)) * outer(y, lambda_before, dpois) / replicate(k, rowSums(   t(replicate(n, pi_before))*outer(y, lambda_before, dpois)  )  )
+    
+    # M-step
+    pi_after = colSums(w)/n
+    lambda_after = colSums(w*y)/colSums(w)
+    
+    iter = iter + 1
+    cat('Iteration: ', iter, '\n')
+  }
+  
+  # log likelihood
+  w = t(replicate(n, pi_after)) * outer(y, lambda_after, dpois) / replicate(k, rowSums(t(replicate(n, pi_after)) * outer(y, lambda_after, dpois)))
+  loglik = sum(w * log(t(replicate(n, pi_after)) * outer(y, lambda_after, dpois)))
+  
+  return(list(iter = iter, loglik = loglik, init = init, pi = pi_after, lambda = lambda_after, wp = w))
+}
