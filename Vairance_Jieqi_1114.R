6eb9e65595cd4154ade2f8ca14e96b1a0d77a111
qiqipower029 <tjqjasmine1029@gmail.com>
2020-11-17 22:14:08 -0600

    psi matrix completed

--- Vairance_Jieqi_1114.R
+++ Vairance_Jieqi_1114.R
@@ -3,11 +3,11 @@
 
 # First, get the point estimates and the W matrix
 library(magrittr)
-k = 2
+k = 4
 # Data Generation
 n = 10000                    
-pi.TRUE = c(0.2, 0.8) 
-lambda.TRUE = c(1, 10)   
+pi.TRUE = c(0.2, 0.2, 0.3, 0.3) 
+lambda.TRUE = c(1, 10, 20, 28)   
 set.seed(1)
 z.TRUE = sample(1:k, size = n, replace = T, prob = pi.TRUE) 
 y = sapply(z.TRUE, function(z) rpois(1, lambda.TRUE[z])) 
@@ -49,5 +49,64 @@ mixture_poisson = function(y, init, tol = 1e-06, max.iter = 100000000) {
   
   return(list(iter = iter, loglik = loglik, init = init, pi = pi_after, lambda = lambda_after, wp = w))
 }
-mixture_poisson(y, init)
-#--------Calculate Psi11-------------#
+result = mixture_poisson(y, init)
+w.matrix = result$wp
+p.est = result$pi
+lambda.est = result$lambda
+
+#--------Variance function-------------#
+variance.function.poisson = function(w.matrix, p.est, lambda.est, y) {
+  n = nrow(w.matrix)
+  k = ncol(w.matrix)
+  # -----psi 11-----#
+  psi.11 = matrix(nrow = k-1, ncol = k-1)
+  for (l in 1:k-1) {
+    
+    b = l+1
+    if(b<k){
+    # off-diagonal entry
+      for (j in b:k-1) {
+        psi.11[l,j] = sum(-w.matrix[,l]*w.matrix[,j]/(p.est[l]*p.est[j])
+                        +w.matrix[,l]*w.matrix[,k]/(p.est[l]*p.est[k])
+                        +w.matrix[,k]*w.matrix[,j]/(p.est[k]*p.est[j])
+                        +w.matrix[,k]*(1-w.matrix[,k])/(p.est[k])^2)
+        psi.11[j,l] = psi.11[l,j]
+      }
+    } 
+    # diagonal entry
+    psi.11[l,l] = sum(w.matrix[,l]*(1-w.matrix[,l])/(p.est[l])^2 
+                      + w.matrix[,k]*(1-w.matrix[,k])/(p.est[k])^2 
+                      + 2*w.matrix[,l]*w.matrix[,k]/(p.est[l]*p.est[k]))
+  }
+  
+  # -----psi 22-----#
+  psi.22 = matrix(nrow = k, ncol = k)
+  for (l in 1:k) {
+    b = l+1
+    if(b<=k){
+      # off-diagonal entry
+      for (j in b:k) {
+        psi.22[l,j] = sum((y/lambda.est[l]-1)*(y/lambda.est[j]-1)*(-w.matrix[,l]*w.matrix[,j]))
+        psi.22[j,l] = psi.22[l,j]
+      }
+    }
+    # diagonal entry
+    psi.22[l,l] = sum((y/lambda.est[l]-1)^2*w.matrix[,l]*(1-w.matrix[,l]))
+  }
+  
+  # -----psi 12-----#
+  psi.12 = matrix(nrow = k-1, ncol = k)
+  for (l in 1:k-1) {
+    b = l+1
+  
+      # off-diagonal entry
+      for (j in b:k) {
+        psi.12[l,j] = sum((y/lambda.est[j]-1)*(-w.matrix[,l]*w.matrix[,j]/p.est[l]+w.matrix[,k]*w.matrix[,j]/p.est[k]))
+        if(j<k) {psi.12[j,l] = psi.12[l,j]}
+      }
+    
+    # diagonal entry
+    psi.12[l,l] = sum((y/lambda.est[l]-1)*(w.matrix[,l]*(1-w.matrix[,l])/p.est[l]+w.matrix[,l]*w.matrix[,k]/p.est[k]))
+  }
+  psi.21 = t(psi.12)
+}
