fe2b8d07072e8b2d47a191792aa88621b43bf9ee
qiqipower029 <tjqjasmine1029@gmail.com>
2020-11-17 18:15:34 -0600

    code update

--- Vairance_Jieqi_1114.R
+++ Vairance_Jieqi_1114.R
@@ -1,7 +1,53 @@
 #-----------------Variance Estimation-----------------#
 #----------------------Jieqi--------------------------#
 
+# First, get the point estimates and the W matrix
+library(magrittr)
 k = 2
+# Data Generation
+n = 10000                    
+pi.TRUE = c(0.2, 0.8) 
+lambda.TRUE = c(1, 10)   
+set.seed(1)
+z.TRUE = sample(1:k, size = n, replace = T, prob = pi.TRUE) 
+y = sapply(z.TRUE, function(z) rpois(1, lambda.TRUE[z])) 
+set.seed(1)
+cluster.kmeans = kmeans(y, centers = k)
+init = list(pi = as.numeric(table(cluster.kmeans$cluster)/n)[order(cluster.kmeans$centers[,1])],
+            lambda = as.numeric(sort(cluster.kmeans$centers[,1])))
+mixture_poisson = function(y, init, tol = 1e-06, max.iter = 100000000) {
+  n = length(y)
+  k = length(init$pi)
+  
+  # E-step
+  pi_after      = init$pi
+  lambda_after  = init$lambda
+  
+  # M-step
+  pi_before     = numeric(k)
+  lambda_before = numeric(k)
+  
+  iter = 0
+  while ((any(abs(lambda_before - lambda_after)/(lambda_before + 1e-04) > tol) | any(abs(pi_before - pi_after)/(pi_before + 1e-04) > tol)) & iter < max.iter) {
+    # E-step
+    pi_before = pi_after
+    lambda_before = lambda_after
 
-
-#--------Calculate Phi11-------------#
+    w = t(replicate(n, pi_before)) * outer(y, lambda_before, dpois) / replicate(k, rowSums(   t(replicate(n, pi_before))*outer(y, lambda_before, dpois)  )  )
+    
+    # M-step
+    pi_after = colSums(w)/n
+    lambda_after = colSums(w*y)/colSums(w)
+    
+    iter = iter + 1
+    cat('Iteration: ', iter, '\n')
+  }
+  
+  # log likelihood
+  w = t(replicate(n, pi_after)) * outer(y, lambda_after, dpois) / replicate(k, rowSums(t(replicate(n, pi_after)) * outer(y, lambda_after, dpois)))
+  loglik = sum(w * log(t(replicate(n, pi_after)) * outer(y, lambda_after, dpois)))
+  
+  return(list(iter = iter, loglik = loglik, init = init, pi = pi_after, lambda = lambda_after, wp = w))
+}
+mixture_poisson(y, init)
+#--------Calculate Psi11-------------#
